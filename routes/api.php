9d4d01127413d01ccfed8067ac7454d6288423c8
Imran Haider <sheensoltech@gmail.com>
2022-12-06 05:58:09 +0500

    updated filters

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-	Route::get('/get-worldpop', [ApiController::class,'getApiData']);
+Route::get('/get-worldpop', [ApiController::class,'getApiData']);
 
